34d556fd3b81f65b79636335e01f66ddc12db269
socialcredit64 <124119894+socialcredit64@users.noreply.github.com>
2023-11-16 16:08:43 -0600

    Add files via upload

--- dbcode.py
+++ dbcode.py
@@ -18,6 +18,19 @@ songs = ""
 
 filelist=list();
 
+# A database using sqlite
+conn = sqlite3.connect("songs.db")
+c = conn.cursor()
+
+c.execute("""CREATE TABLE songs(
+        song_name   text,
+        bpm         real, 
+        
+)""")
+
+conn.commit()
+conn.close()
+
 def openFolder():
     global currentDirectory;
     global text1;
