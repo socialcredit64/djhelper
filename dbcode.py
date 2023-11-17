2331b62c55edc3571ecc4e09030dfb081f3ddfe7
socialcredit64 <124119894+socialcredit64@users.noreply.github.com>
2023-11-17 00:32:21 -0600

    Add files via upload

--- dbcode.py
+++ dbcode.py
@@ -22,14 +22,14 @@ filelist=list();
 conn = sqlite3.connect("songs.db")
 c = conn.cursor()
 
-c.execute("""CREATE TABLE songs(
+'''c.execute("""CREATE TABLE songs(
         song_name   text,
         bpm         real, 
         
-)""")
+        
+)""")'''
 
-conn.commit()
-conn.close()
+
 
 def openFolder():
     global currentDirectory;
@@ -252,4 +252,5 @@ root.title("DJTool")
 root.resizable(False, False)
 root.mainloop()
 
-
+conn.commit()
+conn.close()
